25a2df4a0341501806076efa98ce04b59219145a
Adrian Fisher <75593398+adrian-g-fisher@users.noreply.github.com>
2024-04-04 16:44:16 +1100

    Update lazTile.py

--- lazTile.py
+++ lazTile.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 """
 
-conda create -n wbt -c conda-forge whitebox
+conda create -n wbt -c conda-forge whitebox laspy lazrs-python
+
+x672700_y6073500.laz 672700 6072900 200 700
 
 """
 
@@ -9,28 +11,82 @@ import os
 import sys
 import argparse
 import glob
+import numpy
+import laspy
+import math
+import shutil
 from whitebox.whitebox_tools import WhiteboxTools
+
+
+def append_to_las(in_las, out_las):
+    """
+    Appends in_laz points to out_laz
+    """
+    with laspy.open(out_las, mode='a') as outlas:
+        with laspy.open(in_las) as inlas:
+            for points in inlas.chunk_iterator(2_000_000):
+                outlas.append_points(points)
 
 
 def main(inFile, inDir, outDir, tileSize):
     """
     Main function to tile the data
     """
+    # Get all input files
     if inFile is None:
         tempList = glob.glob(os.path.join(inDir, "*"))
         lasList = []
         for t in tempList:
             if t[-3:] in ['las', 'laz', 'LAS', 'LAZ']:
                 lasList.append(t)
+    else:
+        lasList = [inFile]
+        inDir = os.path.dirname(inFile)
     
-    singleLaz = os.path.join(outDir, "merged.laz")
-    wbt = WhiteboxTools()
-    wbt.lidar_join(';'.join(lasList), singleLaz)
-
     # Tile files
-    wbt = WhiteboxTools()
-    wbt.lidar_tile(singleLaz, width=tileSize, height=tileSize, 
-                   origin_x=0.0, origin_y=0.0, min_points=0)
+    #for lasFile in lasList:
+    #    wbt = WhiteboxTools()
+    #    wbt.set_verbose_mode(False)
+    #    wbt.lidar_tile(lasFile, width=tileSize, height=tileSize, 
+    #                   origin_x=0.0, origin_y=0.0, min_points=0)
+    
+    # Get all tile names
+    tileList = []
+    for lasName in lasList:
+        subdir = lasName[:-4]
+        tileList += glob.glob(os.path.join(subdir, '*.laz'))
+    
+    # Get the extents of each tile
+    xyList = []
+    for lasFile in tileList:
+        las = laspy.read(lasFile)
+        x_min = int(numpy.min(las.x) / tileSize) * tileSize
+        y_min = int(numpy.min(las.y) / tileSize) * tileSize
+        xyList.append('x%i_y%i'%(x_min, y_min))
+    tileList = numpy.array(tileList)
+    xyList = numpy.array(xyList)
+    
+    # Merge or copy tiles into outDir
+    for xy in numpy.unique(xyList):
+        outLas = os.path.join(outDir, '%s.laz'%xy)
+        t = tileList[xyList == xy]
+        if len(t) == 1:
+            shutil.copyfile(t[0], outLas)
+        else:
+            shutil.copyfile(t[0], outLas)
+            for inLas in t[1:]:
+                append_to_las(inLas, outLas)
+    
+    # Check the extents of the final tiles
+    for lasFile in glob.glob(os.path.join(outDir, "*.laz")):
+        las = laspy.read(lasFile)
+        x_min = int(numpy.min(las.x) / tileSize) * tileSize
+        y_min = int(numpy.min(las.y) / tileSize) * tileSize
+        x_max = math.ceil(numpy.max(las.x) / tileSize) * tileSize
+        y_max = math.ceil(numpy.max(las.y) / tileSize) * tileSize
+        x_size = x_max - x_min
+        y_size = y_max - y_min
+        print(os.path.basename(lasFile), x_min, y_min, x_size, y_size)
 
 
 def getCmdargs():
