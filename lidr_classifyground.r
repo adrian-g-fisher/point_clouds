fc6e223686899c041821caa0350938d7e9aa094a
Adrian Fisher <adrian.fisher@unsw.edu.au>
2024-05-29 12:42:00 +1000

    Update lidr_classifyground.r

--- lidr_classifyground.r
+++ lidr_classifyground.r
@@ -1,8 +1,43 @@
-# conda create -n lidr -c conda-forge r-lidr r-future r-rgdal r-codetools r-optparse
+# conda create -n lidr -c conda-forge r-lidr r-future r-rgdal r-codetools r-optparse r-dplyr
 
 library(lidR)
 library(optparse)
 library(future)
+suppressPackageStartupMessages(library(dplyr))
+
+
+lowest <- function(res=1) {
+  stopifnot(is.numeric(res), length(res) == 1L, res > 0)
+  
+  f <- function(las) {
+    r <- grid_metrics(las, ~.I[which.min(Z)], res=res)
+    return(na.omit(r[]))
+  }
+  
+  class(f) <- lidR:::LIDRALGORITHMDEC # Using ::: because not public yet
+  return(f)
+}
+
+
+minZ_pmf <- function(las, res=0.1) {
+  if (is(las, "LAS")) {
+    minZ <- decimate_points(las, lowest(res))
+	ws <- seq(3, 12, 3)
+	th <- seq(0.1, 1.5, length.out=length(ws))
+	minZ <- classify_ground(minZ, algorithm=pmf(ws=ws, th=th))
+	joined <- las@data %>% left_join(minZ@data, by=c('X','Y','ReturnNumber'))
+	joined[is.na(joined)] <- 0
+	las$Classification <- as.integer(joined$Classification.y)
+	return(las)
+  }
+  
+  if (is(las, "LAScatalog")) {
+    options <- list(need_output_file = TRUE, need_buffer = TRUE)
+    las <- catalog_map(las, minZ_pmf, res=res)
+    return(las)
+  }
+}
+
 
 option_list = list(
 	make_option(c("-i", "--inDir"), type="character", default=NULL, 
@@ -23,10 +58,8 @@ inDir <- opt$inDir
 outDir <- opt$outDir
 
 ctg = readLAScatalog(inDir)
-plan(multisession, workers=8L) # 8 cores
+plan(multisession, workers=8)
 opt_chunk_buffer(ctg) <- 20
 opt_laz_compression(ctg) <- TRUE
 opt_output_files(ctg) <- paste0(outDir, "/{*}_classified")
-ws <- seq(3, 12, 3)
-th <- seq(0.1, 1.5, length.out=length(ws))
-ctg <- classify_ground(ctg, algorithm=pmf(ws=ws, th=th))
+output <- minZ_pmf(ctg, res=0.1)
